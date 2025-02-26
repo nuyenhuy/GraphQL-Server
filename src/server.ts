6b2209d5cb12bff520ed989ca1706290f58d93b2
huy.nguyen3 <mailto:huy.nguyen3@fssc.com.vn>
2025-02-27 00:21:54 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,67 +1,76 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import {ApolloServer} from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
 import cors from 'cors';
-import express, {NextFunction} from 'express';
-import { AppContext, UserContext } from './types/types.js';
-import { EventResolver } from './resolvers/event-resolver.js';
-import { AuthChecker, buildSchema } from 'type-graphql';
-import { VenueResolver } from './resolvers/venue-resolver.js';
-import { UserResolver } from './resolvers/user-resolver.js';
+import express from 'express';
+import {AdjCustomer, AppContext, UserContext} from './types/types.js';
+import {EventResolver} from './resolvers/event-resolver.js';
+import {AuthChecker, buildSchema} from 'type-graphql';
+import {VenueResolver} from './resolvers/venue-resolver.js';
+import {UserResolver} from './resolvers/user-resolver.js';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './constants/constants.js';
-import { BookingResolver } from './resolvers/booking-resolver.js';
-import { createServer } from 'http';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { mergeSchemas } from '@graphql-tools/schema';
-import { WebSocketServer } from 'ws';
-import { useServer } from 'graphql-ws/lib/use/ws';
-import { subScriptionSchema } from './subScriptionSchema.js';
-import { ArtistResolver } from './resolvers/artist-resolver.js';
+import {JWT_SECRET} from './constants/constants.js';
+import {BookingResolver} from './resolvers/booking-resolver.js';
+import {createServer} from 'http';
+import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer';
+import {mergeSchemas} from '@graphql-tools/schema';
+import {WebSocketServer} from 'ws';
+import {useServer} from 'graphql-ws/lib/use/ws';
+import {subScriptionSchema} from './subScriptionSchema.js';
+import {ArtistResolver} from './resolvers/artist-resolver.js';
 import {DimCustomerResolver} from "./resolvers/dim-customer-resolver.js";
 import {AdjCustomerResolver} from "./resolvers/adj-customer-resolver.js";
 import {TableResolver} from "./resolvers/table-resolver.js";
-import multer from "multer";
+import {AppDataSource} from "./connection/datasource.js";
+
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+import multer from 'multer';
+import path from "path";
+import xlsx from 'xlsx';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// import multer from "multer";
 const PORT = 3001;
 const app = express();
-
+app.use(cors())
 const httpServer = createServer(app);
 
-const authChecker: AuthChecker<AppContext> = ({ context },roles) => {
+const authChecker: AuthChecker<AppContext> = ({context}, roles) => {
   return context?.userContext?.id &&
-    (roles.length===0 || roles.includes(context.userContext.role));
+    (roles.length === 0 || roles.includes(context.userContext.role));
 };
 
 const typeGraphQLSchema = await buildSchema({
-    resolvers: [EventResolver,VenueResolver,UserResolver,BookingResolver,ArtistResolver,DimCustomerResolver,AdjCustomerResolver,TableResolver],
-    emitSchemaFile: true,
-    authChecker
+  resolvers: [EventResolver, VenueResolver, UserResolver, BookingResolver, ArtistResolver, DimCustomerResolver, AdjCustomerResolver, TableResolver],
+  emitSchemaFile: true,
+  authChecker
 });
 
-const schema = mergeSchemas({schemas: [typeGraphQLSchema,subScriptionSchema]});
+const schema = mergeSchemas({schemas: [typeGraphQLSchema, subScriptionSchema]});
 
 const wsServer = new WebSocketServer({
   server: httpServer,
   path: '/graphql',
 });
 
-const serverCleanup = useServer({ 
-  schema, 
+const serverCleanup = useServer({
+  schema,
   onConnect: (context) => {
     const authorization = context.connectionParams.Authorization || '';
     try {
-        const userContext = jwt.verify(authorization, JWT_SECRET);
-        return {userContext};
+      const userContext = jwt.verify(authorization, JWT_SECRET);
+      return {userContext};
+    } catch (e) {
+      throw new Error('Not authenticated');
     }
-    catch (e) {
-      throw new Error('Not authenticated');          
-    }
-},
+  },
 }, wsServer);
-    
+
 const server = new ApolloServer({
   schema,
   plugins: [
-    ApolloServerPluginDrainHttpServer({ httpServer }),
+    ApolloServerPluginDrainHttpServer({httpServer}),
     {
       async serverWillStart() {
         return {
@@ -77,46 +86,70 @@ const server = new ApolloServer({
 
 await server.start();
 
-const options =   {
+const options = {
   context:
-    async ({ req }):Promise<AppContext> => {
+    async ({req}): Promise<AppContext> => {
       const token = req.headers.authorization || '';
       try {
-        const userContext:UserContext = jwt.verify(token, JWT_SECRET);
+        const userContext: UserContext = jwt.verify(token, JWT_SECRET);
         return {userContext};
       } catch (e) {
-        return {userContext:null};
-      }     
+        return {userContext: null};
+      }
     }
-  };
+};
 
 app.use(
-    "/graphql",
-    cors<cors.CorsRequest>(),
-    express.json(),
-    expressMiddleware(server, options)
+  "/graphql",
+  cors<cors.CorsRequest>(),
+  express.json(),
+  expressMiddleware(server, options)
 );
-httpServer.listen({ port: PORT }, () => {
+httpServer.listen({port: PORT}, () => {
   console.log(`Server ready at http://localhost:${PORT}/graphql`)
 });
 
-// // Cấu hình multer để lưu file tạm thời
-// const upload = multer({ dest: 'uploads/' });
-//
-// import express from 'express';
-// import multer from 'multer';
-// import path from "path";
-//
-//
-// app.post('/upload', upload.single('file'), (req, res) => {
-//     if (!req.file) {
-//         return res.status(400).json({ error: 'No file uploaded' });
-//     }
-//
-//     const filePath = path.join(__dirname, 'uploads', req.file.filename);
-//     res.json({ message: 'File uploaded successfully', path: filePath });
-// });
+// Cấu hình multer để lưu file tạm thời
+const upload = multer({dest: 'uploads/'});
 
+app.post('/upload', upload.single('file'), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({error: 'No file uploaded'});
+  }
 
-// Đảm bảo sử dụng middleware đúng cách
-// app.post('/upload', uploadHandler);
+  // Kiểm tra định dạng file
+  const allowedExtensions = ['.xls', '.xlsx'];
+  const fileExtension = path.extname(req.file.originalname).toLowerCase();
+  if (!allowedExtensions.includes(fileExtension)) {
+    return res.status(400).json({error: 'Invalid file format. Only .xls and .xlsx allowed'});
+  }
+
+  try {
+    // Đọc dữ liệu từ file Excel
+    const workbook = xlsx.readFile(req.file.path);
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const jsonData = xlsx.utils.sheet_to_json(sheet);
+
+    // Chuyển dữ liệu thành danh sách AdjCustomer
+    let customers = jsonData.map((row: any) => {
+      return {
+        customerId: row.customerId,
+        name: row.name,
+        address: row.address,
+        phone: row.phone,
+        email: row.email,
+        createAt: new Date(row.createAt), // Chuyển đổi ngày tháng
+        isMerge: row.isMerge === 'TRUE' // Chuyển boolean
+      };
+    });
+
+    customers = customers.filter(customer => customer.customerId != undefined);
+    const customerRepo = AppDataSource.getRepository(AdjCustomer);
+    await customerRepo.save(customers);
+    res.json({message: 'File processed successfully', totalRecords: customers.length});
+  } catch (error) {
+    console.error('Error processing file:', error);
+    res.status(500).json({error: 'Internal server error'});
+  }
+});
